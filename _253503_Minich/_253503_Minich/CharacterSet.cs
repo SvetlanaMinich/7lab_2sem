b33d49e98062d600210c56ba41356193e4fb15de
SvetlanaMinich <imsvetam@mail.ru>
2023-05-22 02:24:05 +0300

    lab7 sem2 c#

---
+++ _253503_Minich/_253503_Minich/CharacterSet.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _253503_Minich
+{
+    internal class CharacterSet
+    {
+        private List<char> _characters;
+        public CharacterSet(IEnumerable<char> characters)
+        {
+            if (characters == null)
+            {
+                throw new ArgumentNullException(nameof(characters));
+            }
+
+            _characters = new List<char>(characters.Distinct());
+        }
+        public IEnumerable<char> Characters => _characters;
+        public bool IsValidSet()
+        {
+            return _characters.Count == _characters.Distinct().Count();
+        }
+        public static CharacterSet operator +(CharacterSet set1, CharacterSet set2)
+        {
+            var mergedCharacters = set1.Characters.Union(set2.Characters);
+            return new CharacterSet(mergedCharacters);
+        }
+        public static CharacterSet operator -(CharacterSet set1, CharacterSet set2)
+        {
+            var differenceCharacters = set1.Characters.Except(set2.Characters);
+            return new CharacterSet(differenceCharacters);
+        }
+        public static CharacterSet operator *(CharacterSet set1, CharacterSet set2)
+        {
+            var intersectionCharacters = set1.Characters.Intersect(set2.Characters);
+            return new CharacterSet(intersectionCharacters);
+        }
+        public static CharacterSet operator ++(CharacterSet set)
+        {
+           for (int i = 0; i < set._characters.Count; i++)
+            {
+                set._characters[i]++;
+            }
+
+            return set;
+        }
+        public static bool operator ==(CharacterSet set1, CharacterSet set2)
+        {
+            if (set1 is null || set2 is null)
+            {
+                return false;
+            }
+            if (ReferenceEquals(set1, set2))
+            {
+                return true;
+            }
+            return set1.Characters.SequenceEqual(set2.Characters);
+        }
+        public static CharacterSet operator --(CharacterSet set)
+        {
+            for (int i = 0; i < set._characters.Count; i++)
+            {
+                set._characters[i]--;
+            }
+
+            return set;
+        }
+        public static bool operator !=(CharacterSet set1, CharacterSet set2)
+        {
+            return !(set1 == set2);
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is CharacterSet otherSet)
+            {
+                return this == otherSet;
+            }
+
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return _characters?.GetHashCode() ?? 0;
+        }
+        public override string ToString()
+        {
+            return new string(_characters.ToArray());
+        }
+        public static explicit operator CharacterSet(string str)
+        {
+            return new CharacterSet(str);
+        }
+    }
+}
---
+++ _253503_Minich/_253503_Minich/Program.cs
@@ -0,0 +1,29 @@
+﻿using _253503_Minich;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CharacterSet set1 = new CharacterSet("abc");
+        Console.WriteLine("Characters: " + string.Join("", set1.Characters));
+        bool isValid = set1.IsValidSet();
+        Console.WriteLine("Is valid set? " + isValid);
+        CharacterSet set2 = new CharacterSet("bcd");
+        CharacterSet union = set1 + set2;
+        CharacterSet intersection = set1 * set2;
+        CharacterSet difference = set1 - set2;
+        Console.WriteLine("Union: " + string.Join("", union.Characters));
+        Console.WriteLine("Intersection: " + string.Join("", intersection.Characters));
+        Console.WriteLine("Difference: " + string.Join("", difference.Characters));
+        bool areEqual = set1.Equals(set2);
+        Console.WriteLine("Are equal? " + areEqual);
+        string setString = set1.ToString();
+        Console.WriteLine("Set string: " + setString);
+        CharacterSet newSet = (CharacterSet)"xyz";
+        Console.WriteLine("New set characters: " + string.Join("", newSet.Characters));
+        set1++;
+        set2--;
+        Console.WriteLine("Set1 after increment: " + string.Join("", set1.Characters));
+        Console.WriteLine("Set2 after decrement: " + string.Join("", set2.Characters));
+    }
+}
